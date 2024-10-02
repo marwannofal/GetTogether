9fe1c8722e08b59bc7ec14e6bc8f9583d17d81bf
marwannofal <marwannofal64@gmail.com>
2024-10-02 15:25:57 +0300

    end of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
@@ -19,6 +18,8 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({ declarations: [
         AppComponent,
@@ -31,7 +32,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
         TestsErrorComponent,
         NotFoundComponent,
         ServerErrorComponent,
-        MemberCardComponent
+        MemberCardComponent,
+        MemberEditComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
         AppRoutingModule,
@@ -40,6 +42,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
         SharedModule], providers: [
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
         provideHttpClient(withInterceptorsFromDi())
     ] })
 export class AppModule { }
